1a4031aacdac6b3f798fc9b505fd6ea608be913f
Guerson <ultim8p@gmail.com>
2023-03-29 11:01:33 +0200

    Fix package.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/ultim8p/NoVaporAPI.git", from: "0.0.1"),
+        .package(url: "https://github.com/ultim8p/NoVaporAPI.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
