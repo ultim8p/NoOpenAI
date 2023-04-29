f2d1016e0311f1b95d463275f493e686e68fa4e8
Guerson <ultim8p@gmail.com>
2023-04-29 09:56:23 +0300

    Added GPT-4

--- Sources/NoOpenAI/OpenAIChatModel.swift
+++ Sources/NoOpenAI/OpenAIChatModel.swift
@@ -12,4 +12,12 @@ public enum OpenAIChatModel: String, Codable {
     case gpt35turbo = "gpt-3.5-turbo"
     
     case gpt35turbo0301 = "gpt-3.5-turbo-0301"
+    
+    case gpt4 = "gpt-4"
+    
+    case gpt40314 = "gpt-4-0314"
+    
+    case gpt432k = "gpt-4-32k"
+    
+    case gpt432k0314 = "gpt-4-32k-0314"
 }
