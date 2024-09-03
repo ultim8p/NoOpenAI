13c11ab4845d62cfdad051b7807335b6016cdac3
Guerson <ultim8p@gmail.com>
2024-09-03 00:58:38 -0300

    Update to Swift 5.10 & fix Logger dependency.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,6 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/ultim8p/NoVaporAPI.git", branch: "main"),
+        .package(url: "https://github.com/ultim8p/NoLogger.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,6 +27,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NoVaporAPI", package: "NoVaporAPI"),
+                .product(name: "NoLogger", package: "NoLogger"),
             ]),
         .testTarget(
             name: "NoOpenAITests",
